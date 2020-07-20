4efd8741c976fd4d96d4ae876cae355b19e15b8a
Benjamin Cremer <bc@benjamin-cremer.de>
2020-07-20 11:47:54 +0200

    Wrap configuration into an Immediately-Invoked Function Expression to not leak any variables into the configfile scope

--- config/module.config.php
+++ config/module.config.php
@@ -7,102 +7,104 @@
  |
  */
 
-    use Go\Core\AspectContainer;
-    use Go\Core\AspectKernel;
-    use Go\Core\GoAspectContainer;
-    use Go\Laminas\Framework\Factory\AspectContainerFactory;
-    use Go\Laminas\Framework\Factory\AspectKernelFactory;
+use Go\Core\AspectContainer;
+use Go\Core\AspectKernel;
+use Go\Core\GoAspectContainer;
+use Go\Laminas\Framework\Factory\AspectContainerFactory;
+use Go\Laminas\Framework\Factory\AspectKernelFactory;
 
+return (static function (): array {
     $basicDirectory = defined('APPLICATION_PATH') ? APPLICATION_PATH : __DIR__ . '/../../../..';
 
-$moduleConfig = [
-    /*
-     |--------------------------------------------------------------------------
-     | AOP Debug Mode
-     |--------------------------------------------------------------------------
-     |
-     | When AOP is in debug mode, then breakpoints in the original source code
-     | will work. Also engine will refresh cache files if the original files were
-     | changed.
-     | For production mode, no extra filemtime checks and better integration with opcache
-     |
-     */
-    'debug' => true,
+    $moduleConfig = [
+        /*
+         |--------------------------------------------------------------------------
+         | AOP Debug Mode
+         |--------------------------------------------------------------------------
+         |
+         | When AOP is in debug mode, then breakpoints in the original source code
+         | will work. Also engine will refresh cache files if the original files were
+         | changed.
+         | For production mode, no extra filemtime checks and better integration with opcache
+         |
+         */
+        'debug' => true,
 
-    /*
-     |--------------------------------------------------------------------------
-     | Application root directory
-     |--------------------------------------------------------------------------
-     |
-     | AOP will be applied only to the files in this directory, change it if needed
-     */
-    'appDir' => $basicDirectory,
+        /*
+         |--------------------------------------------------------------------------
+         | Application root directory
+         |--------------------------------------------------------------------------
+         |
+         | AOP will be applied only to the files in this directory, change it if needed
+         */
+        'appDir' => $basicDirectory,
 
-    /*
-     |--------------------------------------------------------------------------
-     | AOP cache directory
-     |--------------------------------------------------------------------------
-     |
-     | AOP engine will put all transformed files and caches in that directory
-     */
-    'cacheDir' => $basicDirectory . '/data/cache/aspect',
+        /*
+         |--------------------------------------------------------------------------
+         | AOP cache directory
+         |--------------------------------------------------------------------------
+         |
+         | AOP engine will put all transformed files and caches in that directory
+         */
+        'cacheDir' => $basicDirectory . '/data/cache/aspect',
 
-    /*
-     |--------------------------------------------------------------------------
-     | Cache file mode
-     |--------------------------------------------------------------------------
-     |
-     | If configured then will be used as cache file mode for chmod
-     */
-    'cacheFileMode' => 0770 & ~umask(),
+        /*
+         |--------------------------------------------------------------------------
+         | Cache file mode
+         |--------------------------------------------------------------------------
+         |
+         | If configured then will be used as cache file mode for chmod
+         */
+        'cacheFileMode' => 0770 & ~umask(),
 
-    /*
-     |--------------------------------------------------------------------------
-     | Controls miscellaneous features of AOP engine
-     |--------------------------------------------------------------------------
-     |
-     | See \Go\Aop\Features enumeration for bit mask
-     */
-    'features' => 0,
+        /*
+         |--------------------------------------------------------------------------
+         | Controls miscellaneous features of AOP engine
+         |--------------------------------------------------------------------------
+         |
+         | See \Go\Aop\Features enumeration for bit mask
+         */
+        'features' => 0,
 
-    /*
-     |--------------------------------------------------------------------------
-     | White list of directories
-     |--------------------------------------------------------------------------
-     |
-     | AOP will check this list to apply an AOP to selected directories only,
-     | leave it empty if you want AOP to be applied to all files in the appDir
-     */
-    'includePaths' => [],
+        /*
+         |--------------------------------------------------------------------------
+         | White list of directories
+         |--------------------------------------------------------------------------
+         |
+         | AOP will check this list to apply an AOP to selected directories only,
+         | leave it empty if you want AOP to be applied to all files in the appDir
+         */
+        'includePaths' => [],
 
-    /*
-     |--------------------------------------------------------------------------
-     | Black list of directories
-     |--------------------------------------------------------------------------
-     |
-     | AOP will check this list to disable AOP for selected directories
-     */
-    'excludePaths' => [],
+        /*
+         |--------------------------------------------------------------------------
+         | Black list of directories
+         |--------------------------------------------------------------------------
+         |
+         | AOP will check this list to disable AOP for selected directories
+         */
+        'excludePaths' => [],
 
-    /**
-     |--------------------------------------------------------------------------
-     | AOP container class
-     |--------------------------------------------------------------------------
-     |
-     | This option can be useful for extension and fine-tuning of services
-     */
-    'containerClass' => GoAspectContainer::class,
-];
+        /**
+         * |--------------------------------------------------------------------------
+         * | AOP container class
+         * |--------------------------------------------------------------------------
+         * |
+         * | This option can be useful for extension and fine-tuning of services
+         */
+        'containerClass' => GoAspectContainer::class,
+    ];
 
-return [
-    \Go\Laminas\Framework\Module::CONFIG_KEY => $moduleConfig,
+    return [
+        \Go\Laminas\Framework\Module::CONFIG_KEY => $moduleConfig,
 
-    'service_manager' => [
-        'factories' => [
-            AspectKernel::class    => AspectKernelFactory::class,
-            AspectContainer::class => AspectContainerFactory::class,
-        ]
-    ],
+        'service_manager' => [
+            'factories' => [
+                AspectKernel::class => AspectKernelFactory::class,
+                AspectContainer::class => AspectContainerFactory::class,
+            ]
+        ],
 
-    \Go\Laminas\Framework\Module::ASPECT_CONFIG_KEY => []
-];
+        \Go\Laminas\Framework\Module::ASPECT_CONFIG_KEY => []
+    ];
+})();
